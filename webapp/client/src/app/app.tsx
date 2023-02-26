0cb52e7927735fa48e004dd070ef64234d708822
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2023-02-26 23:17:34 +0530

    test: husky

--- webapp/client/src/app/app.tsx
+++ webapp/client/src/app/app.tsx
@@ -4,6 +4,8 @@ import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
 
 export function App() {
+     const app = "";
+
   return (
     <>
       <NxWelcome title="client" />
