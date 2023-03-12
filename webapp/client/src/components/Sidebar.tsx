113db113a46ffb779e8133ec9da8d4f49be07fad
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2023-03-12 19:15:49 +0530

    add: icon package and sidebar

--- webapp/client/src/components/Sidebar.tsx
+++ webapp/client/src/components/Sidebar.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { IoHomeOutline, IoExit } from 'react-icons/io5';
+
+import logo from '../assets/logo-large-transparent.png';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="flex flex-col items-center justify-between	">
+      <div className="w-40 h-40">
+        <img
+          className="w-full h-full object-cover"
+          src={logo}
+          alt="Future Reads logo"
+        />
+      </div>
+      <div className="">
+        <div>
+          <IoHomeOutline />
+          <span>Dashboard</span>
+        </div>
+      </div>
+      <div className="">
+        <div>
+          <IoExit />
+          <span>Sign-out</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
